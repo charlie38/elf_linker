0584831d3dcff1949ab26833996436d047b5b27b
Emilien AUFAUVRE <emilien.aufauvre@live.fr>
2019-12-16 11:17:36 +0000

    Fonction pour lire le ELF d'un objet terminée

---
+++ read_elf.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+#include "usr/include/elf.h"
+
+
+Elf32_Ehdr read_header(FILE *f)
+{
+    Elf32_Ehdr elf = malloc(sizeof(Elf32_Ehdr)) ;
+
+    fread(&elf.e_ident[EI_MAG0], 1, 1, f) ; 
+    fread(&elf.e_ident[EI_MAG1], 1, 1, f) ; 
+    fread(&elf.e_ident[EI_MAG2], 1, 1, f) ; 
+    fread(&elf.e_ident[EI_MAG3], 1, 1, f) ; 
+    fread(&elf.e_ident[EI_CLASS], 1, 1, f) ; 
+    fread(&elf.e_ident[EI_DATA], 1, 1, f) ; 
+    fread(&elf.e_ident[EI_VERSION], 1, 1, f) ; 
+    fread(&elf.e_ident[EI_PAD], 1, 1, f) ; 
+    fread(&elf.e_ident[EI_NIDENT], 1, 1, f) ; 
+    fread(&elf.e_type, 1, 1, f) ; 
+    fread(&elf.e_machine, 1, 1, f) ; 
+    fread(&elf.e_version, 1, 1, f) ; 
+    fread(&elf.e_entry, 1, 1, f) ; 
+    fread(&elf.e_phoff, 1, 1, f) ; 
+    fread(&elf.e_shoff, 1, 1, f) ; 
+    fread(&elf.e_flags, 1, 1, f) ; 
+    fread(&elf.e_ehsize, 1, 1, f) ; 
+    fread(&elf.e_phentsize, 1, 1, f) ; 
+    fread(&elf.e_phnum, 1, 1, f) ; 
+    fread(&elf.e_shentsize, 1, 1, f) ; 
+    fread(&elf.e_shnum, 1, 1, f) ; 
+    fread(&elf.e_shstrndx, 1, 1, f) ; 
+
+    return elf ;
+}
---
+++ read_elf.h
@@ -0,0 +1,8 @@
+#ifndef __READ_ELF_H__
+
+#include "usr/include/elf.h"
+
+
+Elf32_Ehdr read_header(FILE *f) ;
+
+#define __READ_ELF_H__
