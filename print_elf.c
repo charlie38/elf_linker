86c4b1fd9fead5d53460dff88fefc23f5d354c19
Emilien AUFAUVRE <emilien.aufauvre@live.fr>
2019-12-16 12:52:19 +0000

    Affichage de l'ELF presque terminé (reste à completer la fonction pour la version ABI), non testé

--- print_elf.c
+++ print_elf.c
@@ -5,18 +5,14 @@
 #include "/usr/include/elf.h"
 
 
-#define BUFFER
-
-
 void print_header(Elf32_Ehdr elf)
 {
-    printf("Ficher: %s\n", ) ; // TODO
-    printf("En-tête ELF:") ;
+    printf("ELF Header:\n") ;
 }
 
 void print_magic(Elf32_Ehdr elf)
 {
-    printf("  Magique:  ") ;
+    printf("  Magic:  ") ;
 
     int i ;
     
@@ -30,7 +26,7 @@ void print_magic(Elf32_Ehdr elf)
 
 void print_class(Elf32_Ehdr elf)
 {
-    printf("  Classe:  ", ); // TODO
+    printf("  Class:                             ") ;
     
     switch (elf.e_ident[EI_CLASS])
     {
@@ -42,8 +38,8 @@ void print_class(Elf32_Ehdr elf)
 }
 
 void print_data(Elf32_Ehdr elf)
-{
-    printf("  Données: ");
+{          
+    printf("  Data:                              ") ;
 
     switch (elf.e_ident[EI_DATA])
     {
@@ -54,14 +50,15 @@ void print_data(Elf32_Ehdr elf)
     }
 }
 
-void print_version(Elf32_Ehdr elf)
+void print_version_magic(Elf32_Ehdr elf)
 {
-    printf("  Version:  %c",E.e_version) ;
+    printf("  Version:                           %d\n", elf.e_ident[EI_VERSION]) ;
 }
 
 void print_os_abi(Elf32_Ehdr elf)
 {
-    printf("  OS/ABI:  ") ;
+    printf("  OS/ABI:                            ") ;
+
     switch (elf.e_ident[EI_OSABI])
     {
         case ELFOSABI_NONE : printf("UNIX System V ABI\n"); break;
@@ -86,19 +83,118 @@ void print_os_abi(Elf32_Ehdr elf)
 
 void print_abi_version(Elf32_Ehdr elf)
 {
+    printf("  ABI Version:                       ") ;
+
     // TODO
 }
 
 void print_type(Elf32_Ehdr elf)
 {
-    printf("  Type:  ") ;// TODO
+    printf("  Type:                              ") ;
+
+    switch (elf.e_type) 
+    {
+        case ET_NONE : printf("No file type\n") ; break ;
+        case ET_REL : printf("Relocatable file\n") ; break ;
+        case ET_EXEC : printf("Executable file\n") ; break ;
+        case ET_DYN : printf("Shared object file\n") ; break ;
+        case ET_CORE : printf("Core file\n") ; break ;
+        case ET_LOPROC : 
+        case ET_HIPROC : printf("Processor-specific\n") ;
+    }
 }
 
 void print_machine(Elf32_Ehdr elf)
 {
-    printf("  Machine:  ") ;// TODO
+    printf("  Machine:                           ") ;
+
+    switch (elf.e_machine) 
+    {
+        case ET_NONE : printf("No machine\n") ; break ;  
+        case EM_M32 : printf("AT&T WE 32100\n") ; break ;  
+        case EM_SPARC : printf("SPARC\n") ; break ;  
+        case EM_386 : printf("Intel Architecture\n") ; break ;  
+        case EM_68K : printf("Motorola 68000\n") ; break ;  
+        case EM_88K : printf("Motorola 88000\n") ; break ;  
+        case EM_860 : printf("Intel 80860\n") ; break ;  
+        case EM_MIPS : printf("MIPS RS3000 Big-Endian\n") ; break ;  
+        case EM_MIPS_RS4_BE : printf("MIPS RS4000 Big-Endian\n") ; break ;  
+        case RESERVED : printf("Reserved for future use\n") ;
+    }
 }
 
+void print_version(Elf32_Ehdr elf)
+{
+    printf("  Version:                           ") ;
+
+    switch (elf.e_version) 
+    {
+        case EV_NONE : printf("0 (invalid version)\n") ; break ;
+        case EV_CURRENT : printf("1 (current version)\n") ;
+    }
+}
+
+void print_entry(Elf32_Ehdr elf)
+{
+    printf("  Entry point access:                %x\n", elf.e_entry) ;
+}
+
+void print_start_prog_headers(Elf32_Ehdr elf)
+{
+    printf("  Start of program headers:          ") ;
+
+    switch (elf.e_phoff)
+    {
+        case 0 : printf("No program headers table\n") ; break ;
+        default : printf("%d (bytes into file)\n", elf.e_phoff) ;
+    }
+}
+
+void print_start_sect_headers(Elf32_Ehdr elf)
+{
+    printf("  Start of section headers:          ") ;
+
+    switch (elf.e_shoff)
+    {
+        case 0 : printf("No section headers table\n") ; break ;
+        default : printf("%d (bytes into file)\n", elf.e_phoff) ;
+    }
+}
+
+void print_flags(Elf32_Ehdr)
+{
+    printf("  Flags:                             %x\n", elf.e_flags) ;
+}
+
+void print_header_size(Elf32_Ehdr)
+{
+    printf("  Size of this headers:              %d (bytes)\n", elf.e_ehsize) ;
+}
+
+void print_prog_header_size(Elf32_Ehdr)
+{
+    printf("  Size of program headers:           %d (bytes)\n", elf.e_phentsize) ;
+}
+
+void print_prog_header_num(Elf32_Ehdr)
+{
+    printf("  Number of program headers:         %d\n", elf.e_phnum) ;
+}
+
+void print_sect_header_size(Elf32_Ehdr)
+{
+    printf("  Size of section headers:           %d (bytes)\n", elf.e_shentsize) ;
+}
+
+void print_sect_header_num(Elf32_Ehdr)
+{
+    printf("  Number of section headers:         %d\n", elf.e_shnum) ;
+}
+
+void print_table_index(Elf32_Ehdr)
+{
+    printf("  Section header string table index: %d\n", elf.e_shstrndx) ;
+}
 
 void print_elf(Elf32_Ehdr elf)
 { 
@@ -106,7 +202,20 @@ void print_elf(Elf32_Ehdr elf)
     print_magic(elf) ;
     print_class(elf) ;
     print_data(elf) ;
-    print_version(elf) ;
+    print_version_magic(elf) ;
     print_os_abi(elf) ;
-    print_abi_version(elf) ;
+    print_version_abi(elf) ;
+    print_type(elf) ;
+    print_machine(elf) ;
+    print_version(elf) ;
+    print_entry(elf) ;
+    print_start_prog_headers(elf) ;
+    print_start_sect_headers(elf) ;
+    print_flags(elf) ;
+    print_header_size(elf) ;
+    print_prog_header_size(elf) ;
+    print_prog_header_num(elf) ;
+    print_sect_header_size(elf) ;
+    print_sect_header_num(elf) ;
+    print_table_index(elf) ;
 }
---
+++ print_elf.h
@@ -0,0 +1,10 @@
+#include <elf.h>
+#ifndef __PRINT_ELF_H__
+
+#include "usr/include/elf.h"
+
+
+void print_elf(Elf32_Ehdr elf) ;
+
+
+#define __PRINT_ELF_H__
