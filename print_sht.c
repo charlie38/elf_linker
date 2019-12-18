f68cf140baa755fc61721d9d4a99776c1c49d581
Hugo Bantignies <bantignieshugo@hotmail.fr>
2019-12-18 13:10:20 +0000

    Modification etape 2 Modifications des fichiers de l'etape 2. Normalement tout fonctionne sauf le name car on a besoin de la suite.

--- print_sht.c
+++ print_sht.c
@@ -11,7 +11,7 @@ void espaces(int n){
 void afficher_table_sections(Elf32_Shdr TS[], int taille, char strtab[]){
 	int i;
 	char mot[50];
-	printf("[Nr]            Name            Type            Addr      Offs      Size          ES       Flg        Lk       Inf        Al\n");
+	printf("[Nr]            Name            Type            Addr    Offs      Size  ES       Flg  Lk Inf  Al\n");
 	
 	for(i=0; i<taille; i++){
 		if(i<10) printf("[ %d]",i);
@@ -20,91 +20,99 @@ void afficher_table_sections(Elf32_Shdr TS[], int taille, char strtab[]){
     	
 		//printf("Nom : %s  | ", strtab[TS[i].sh_name]);
     	//mot=strtab[TS[i].sh_name];
-    	espaces(16);
+		espaces(16);
     	//printf("%s",mot);
     	
     	//TYPE:
-      	mot[0] = '\0';
+		strcpy(mot, "");
 
-	    switch(TS[i].sh_type){
-	      case SHT_NULL: strcpy(mot,"NULL");
-	      case SHT_PROGBITS: strcpy(mot,"PROGBITS");
-	      case SHT_SYMTAB: strcpy(mot,"SYMTAB");
-	      case SHT_STRTAB: strcpy(mot,"STRTAB");
-	      case SHT_RELA: strcpy(mot,"RELA");
-	      case SHT_HASH: strcpy(mot,"HASH");
-	      case SHT_DYNAMIC: strcpy(mot,"DYNAMIC");
-	      case SHT_NOTE: strcpy(mot,"NOTE");
-	      case SHT_NOBITS: strcpy(mot,"NOBIT");
-	      case SHT_REL: strcpy(mot,"RELA");
-	      case SHT_SHLIB: strcpy(mot,"SHLIB");
-	      case SHT_DYNSYM: strcpy(mot,"DYNSYM");
-	      case SHT_LOPROC: strcpy(mot,"LOPROC");
-	      case SHT_HIPROC: strcpy(mot,"HIPROC");
-	      case SHT_LOUSER: strcpy(mot,"LOUSER");
-	      case SHT_HIUSER: strcpy(mot,"HIUSER");      
-        default: strcpy(mot, "");
-	    }
+		switch(TS[i].sh_type){
+			case SHT_NULL: strcpy(mot,"NULL"); break; 
+			case SHT_PROGBITS: strcpy(mot,"PROGBITS"); break; 
+			case SHT_SYMTAB: strcpy(mot,"SYMTAB"); break; 
+			case SHT_STRTAB: strcpy(mot,"STRTAB"); break; 
+			case SHT_RELA: strcpy(mot,"RELA"); break;  
+			case SHT_HASH: strcpy(mot,"HASH"); break; 
+			case SHT_DYNAMIC: strcpy(mot,"DYNAMIC"); break; 
+			case SHT_NOTE: strcpy(mot,"NOTE"); break; 
+			case SHT_NOBITS: strcpy(mot,"NOBIT"); break; 
+			case SHT_REL: strcpy(mot,"RELA"); break; 
+			case SHT_SHLIB: strcpy(mot,"SHLIB"); break; 
+			case SHT_DYNSYM: strcpy(mot,"DYNSYM"); break; 
+			case SHT_LOPROC: strcpy(mot,"LOPROC"); break; 
+			case SHT_HIPROC: strcpy(mot,"HIPROC"); break; 
+			case SHT_LOUSER: strcpy(mot,"LOUSER"); break; 
+			case SHT_HIUSER: strcpy(mot,"HIUSER");  break;
+			case SHT_ARM_EXIDX: strcpy(mot,"ARM_EXIDX"); break;
+			case SHT_ARM_PREEMPTMAP: strcpy(mot,"ARM_PREEMPTMAP"); break;
+			case SHT_ARM_ATTRIBUTES: strcpy(mot,"ARM_ATTRIBUTES") ; break;
+			default: strcpy(mot, "");
+		}
 	    
-	    espaces(16-strlen(mot));
-    	printf("%s",mot);
+		espaces(16-strlen(mot));
+		printf("%s",mot);
     
     
     	//ADDR
     
-    	espaces(8);
-    	printf("%8.8x", TS[i].sh_addr);
+		espaces(8);
+		printf("%8.8x", TS[i].sh_addr);
     
 		//OFFSET
 		
-    	espaces(2);
-    	printf("%8.8x", TS[i].sh_offset);
+		espaces(2);
+		printf("%6.6x", TS[i].sh_offset);
     
     
     	//SIZE
     
-    	espaces(2);    
-    	printf("%8.8x", TS[i].sh_size);
+		espaces(2);    
+		printf("%8.8x", TS[i].sh_size);
     
         //ENTSIZE
     
-    	espaces(4);
-    	printf("%8.8x", TS[i].sh_entsize);
+		espaces(2);
+		printf("%2.2x", TS[i].sh_entsize);
     
     	
     
     //FLAG
     
-    	
-		  strcpy(mot,"");  
+		    	
+		strcpy(mot,"");  
 		  	    
-	    if( TS[i].sh_flags & SHF_WRITE ) strcat(mot,"W");
-	    if( TS[i].sh_flags & SHF_ALLOC ) strcat(mot,"A");
-	    if( TS[i].sh_flags & SHF_EXECINSTR ) strcat(mot,"I");
-	    //if( TS[i].sh_flags & SHF_MASKPROC ) mot=strcat(mot, "X");
+		if( TS[i].sh_flags & SHF_WRITE ) strcat(mot,"W");
+		if( TS[i].sh_flags & SHF_ALLOC ) strcat(mot,"A");
+		if( TS[i].sh_flags & SHF_EXECINSTR ) strcat(mot,"X");
+		if( TS[i].sh_flags & SHF_MERGE ) strcat(mot, "M");
+		if( TS[i].sh_flags & SHF_STRINGS ) strcat(mot, "S");
+		if( TS[i].sh_flags & SHF_MERGE ) strcat(mot, "M");
+		if( TS[i].sh_flags & SHF_INFO_LINK ) strcat(mot, "I");
 	    
-	    espaces(10-strlen(mot));
-      printf("%s",mot);
+		espaces(10-strlen(mot));
+		printf("%s",mot);
    		
     
 
     
     
     	//LINK
-    	espaces(2);
-    	printf("%8.8x", TS[i].sh_link);
+		espaces(2);
+		if(TS[i].sh_link<10) printf(" %d", TS[i].sh_link);
+		else printf("%d",TS[i].sh_link);
     
     	//INFO
     	
-    	espaces(2);
-    	printf("%8.8x", TS[i].sh_info);
-    
+		espaces(2);
+     
+		if(TS[i].sh_info<10) printf(" %d", TS[i].sh_info);
+		else printf("%d",TS[i].sh_info);
     
     	//ADDRALIGN
-    	espaces(2);
-    	printf("%8.8x", TS[i].sh_addralign);
+		espaces(2);
+		printf("%d", TS[i].sh_addralign);
     
-    	printf("\n");
+		printf("\n");
 
 	}
 	
--- read_sect_elf.c
+++ read_sect_elf.c
@@ -4,6 +4,7 @@
 #include "elf.h"
 #include "read_sect_elf.h"
 #include "read_elf.h"
+#include "elf_linker-1.0/util.h"
 
 void read_section(FILE *f, Elf32_Shdr section_tab[],uint32_t offset,uint32_t nb_sect){
     
@@ -12,16 +13,36 @@ void read_section(FILE *f, Elf32_Shdr section_tab[],uint32_t offset,uint32_t nb_
     fseek(f,offset,SEEK_SET); //Placement de l'indicateur de position au niveau de l'entête de la table des sections
 
     for(i=0;i<nb_sect;i++){
-        fread(&section_tab[i].sh_name,4,1,f);    //Lecture du name
-        fread(&section_tab[i].sh_type,4,1,f);    //Lecture du type
-        fread(&section_tab[i].sh_flags,4,1,f);   //Lecture des flags
-        fread(&section_tab[i].sh_addr,4,1,f);    //Lecture de l'adresse
-        fread(&section_tab[i].sh_offset,4,1,f);  //Lecture de l'offset
-        fread(&section_tab[i].sh_size,4,1,f);     //Lecture de la taille
-        fread(&section_tab[i].sh_link,4,1,f);     //Lecture du lien
-        fread(&section_tab[i].sh_info,4,1,f);    //Lecture de l'info
-        fread(&section_tab[i].sh_addralign,4,1,f);  //Lecutre de l'addralign
-        fread(&section_tab[i].sh_entsize,4,1,f);   //Lecture de l'entsize
+        fread(&section_tab[i].sh_name,sizeof(Elf32_Word),1,f);    //Lecture du name
+        section_tab[i].sh_name=reverse_4(section_tab[i].sh_name);
+
+        fread(&section_tab[i].sh_type,sizeof(Elf32_Word),1,f);    //Lecture du type
+        section_tab[i].sh_type=reverse_4(section_tab[i].sh_type);
+
+        fread(&section_tab[i].sh_flags,sizeof(Elf32_Word),1,f);   //Lecture des flags
+        section_tab[i].sh_flags=reverse_4(section_tab[i].sh_flags);
+
+        fread(&section_tab[i].sh_addr,sizeof(Elf32_Addr),1,f);    //Lecture de l'adresse
+        section_tab[i].sh_addr=reverse_4(section_tab[i].sh_addr);
+
+        fread(&section_tab[i].sh_offset,sizeof(Elf32_Off),1,f);  //Lecture de l'offset
+        section_tab[i].sh_offset=reverse_4(section_tab[i].sh_offset);
+
+        fread(&section_tab[i].sh_size,sizeof(Elf32_Word),1,f);     //Lecture de la taille
+        section_tab[i].sh_size=reverse_4(section_tab[i].sh_size);
+
+        fread(&section_tab[i].sh_link,sizeof(Elf32_Word),1,f);     //Lecture du lien
+        section_tab[i].sh_link=reverse_4(section_tab[i].sh_link);
+        
+        fread(&section_tab[i].sh_info,sizeof(Elf32_Word),1,f);    //Lecture de l'info
+        section_tab[i].sh_info=reverse_4(section_tab[i].sh_info);
+
+        fread(&section_tab[i].sh_addralign,sizeof(Elf32_Word),1,f);  //Lecutre de l'addralign
+        section_tab[i].sh_addralign=reverse_4(section_tab[i].sh_addralign);
+
+        fread(&section_tab[i].sh_entsize,sizeof(Elf32_Word),1,f);   //Lecture de l'entsize
+        section_tab[i].sh_entsize=reverse_4(section_tab[i].sh_entsize);
+
     }
 
     return;
