41e1edf716277a40852e03df7bc5388e7b4143a3
Hugo Bantignies <bantignieshugo@hotmail.fr>
2019-12-18 12:53:54 +0000

    Modification : read_elf.c Fichier qui compromettait l'etape 1 , maintenant , l'étape 1 fonctionne.

--- read_elf.c
+++ read_elf.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 #include "elf.h"
+#include "elf_linker-1.0/util.h"
 
 
 Elf32_Ehdr read_header(FILE *f)
@@ -26,20 +27,45 @@ Elf32_Ehdr read_header(FILE *f)
     fread(&elf.e_ident[13], 1, 1, f) ;
     fread(&elf.e_ident[14], 1, 1, f) ;
     fread(&elf.e_ident[15], 1, 1, f) ; 
-    fread(&elf.e_ident[16], 1, 1, f) ; 
-    fread(&elf.e_type, 2, 1, f) ; 
-    fread(&elf.e_machine, 2, 1, f) ; 
-    fread(&elf.e_version, 4, 1, f) ; 
-    fread(&elf.e_entry, 4, 1, f) ; 
-    fread(&elf.e_phoff, 4, 1, f) ; 
-    fread(&elf.e_shoff, 4, 1, f) ; 
-    fread(&elf.e_flags, 4, 1, f) ; 
-    fread(&elf.e_ehsize, 2, 1, f) ; 
-    fread(&elf.e_phentsize, 2, 1, f) ; 
-    fread(&elf.e_phnum, 2, 1, f) ; 
-    fread(&elf.e_shentsize, 2, 1, f) ; 
-    fread(&elf.e_shnum, 2, 1, f) ; 
-    fread(&elf.e_shstrndx, 2, 1, f) ; 
+    
+    fread(&elf.e_type, sizeof(Elf32_Half), 1, f) ; //Lecture du type
+    elf.e_type = reverse_2(elf.e_type);
+
+    fread(&elf.e_machine, sizeof(Elf32_Half), 1, f) ; //Lecture de la machine
+    elf.e_machine = reverse_2(elf.e_machine);
+
+    fread(&elf.e_version, sizeof(Elf32_Word), 1, f) ; //Lecture de la version
+    elf.e_version = reverse_4(elf.e_version);
+
+    fread(&elf.e_entry, sizeof(Elf32_Addr), 1, f) ; //Lecture de l'entree
+    elf.e_entry = reverse_4(elf.e_entry);
+
+    fread(&elf.e_phoff, sizeof(Elf32_Off), 1, f) ; //Lecture de l'offset ph
+    elf.e_phoff = reverse_4(elf.e_phoff);
+
+    fread(&elf.e_shoff, sizeof(Elf32_Off), 1, f) ; //Lecture de l'offset section header
+    elf.e_shoff = reverse_4(elf.e_shoff);
+
+    fread(&elf.e_flags, sizeof(Elf32_Word), 1, f) ;  //Lecture des flags
+    elf.e_flags = reverse_4(elf.e_flags);
+
+    fread(&elf.e_ehsize, sizeof(Elf32_Half), 1, f) ; //Lecture de la taille de l'entête
+    elf.e_ehsize = reverse_2(elf.e_ehsize);
+
+    fread(&elf.e_phentsize, sizeof(Elf32_Half), 1, f) ;  //Lecture de la taille de ph
+    elf.e_phentsize = reverse_2(elf.e_phentsize);
+    
+    fread(&elf.e_phnum, sizeof(Elf32_Half), 1, f) ;  //Lecture du nombre de ph
+    elf.e_phnum = reverse_2(elf.e_phnum);
+
+    fread(&elf.e_shentsize, sizeof(Elf32_Half), 1, f) ; //Lecture de la taille de section header 
+    elf.e_shentsize = reverse_2(elf.e_shentsize);
+
+    fread(&elf.e_shnum, sizeof(Elf32_Half), 1, f) ; //Lecture du nombre de section
+    elf.e_shnum = reverse_2(elf.e_shnum);
+
+    fread(&elf.e_shstrndx, sizeof(Elf32_Half), 1, f) ; //Lecture de la section 
+    elf.e_shstrndx = reverse_2(elf.e_shstrndx);
 
     return elf ;
 }
