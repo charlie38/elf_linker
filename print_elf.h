75a12f7280f2aa0a9421ddc49f2b2f338ef339f1
Hugo Bantignies <bantignieshugo@hotmail.fr>
2019-12-30 22:13:58 +0000

    Supression des anciens fichiers Suppression des anciens fichiers ( ceux que j'ai actualisé , même au niveau des noms ).

--- print_elf.h
+++
@@ -1,9 +0,0 @@
-#ifndef __PRINT_ELF_H__
-#define __PRINT_ELF_H__
-
-#include "elf.h"
-
-
-void print_elf(Elf32_Ehdr elf) ;
-
-#endif
--- read_elf.h
+++
@@ -1,9 +0,0 @@
-#ifndef __READ_ELF_H__
-#define __READ_ELF_H__
-
-#include "elf.h"
-
-
-Elf32_Ehdr read_header(FILE *f) ;
-
-#endif
