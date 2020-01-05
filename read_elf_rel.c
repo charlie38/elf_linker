adc946aec4d8757b85e91c723b07d950033230c4
Hugo Bantignies <bantignieshugo@hotmail.fr>
2020-01-05 09:23:22 +0000

    Ajout du RELA dans la partie 5 J'ai ajouté le RELA , car c'était pas complet de seulement avoir le REL dans la partie 5.

--- read_elf_rel.c
+++ read_elf_rel.c
@@ -13,8 +13,29 @@ Elf32_Rel read_rel_entry(FILE *f,uint32_t section_offset){
     fread(&rel.r_offset,sizeof(Elf32_Addr),1,f);
     fread(&rel.r_info,sizeof(Elf32_Word),1,f);
 
+    //Selon l'endianess on reverse (ici , il faut le faire)
+
     rel.r_offset = reverse_4(rel.r_offset);
     rel.r_info = reverse_4(rel.r_info);
 
     return rel;
+}
+
+
+Elf32_Rela read_rela_entry(FILE *f,uint32_t section_offset){
+    Elf32_Rela rela;
+
+    fseek(f,section_offset,SEEK_SET);
+
+    fread(&rela.r_offset,sizeof(Elf32_Addr),1,f);
+    fread(&rela.r_info,sizeof(Elf32_Word),1,f);
+    fread(&rela.r_addend,sizeof(Elf32_Sword),1,f);
+
+    //Selon l'endianess on reverse (ici , il faut le faire)
+
+    rela.r_offset = reverse_4(rela.r_offset);
+    rela.r_info = reverse_4(rela.r_info);
+    rela.r_addend = reverse_4(rela.r_addend);
+
+    return rela;
 }
