3013d4aa18a86d8345db3446b49d9b5fa96cac87
Hugo Bantignies <bantignieshugo@hotmail.fr>
2020-01-09 09:36:49 +0000

    Ajout commentaires

--- write_elf_head.h
+++ write_elf_head.h
@@ -1,6 +1,7 @@
-#ifndef WRITE_ELF_HEAD_H
-#define WRITE_ELF_HEAD_H
+#ifndef __WRITE_ELF_HEAD_H__
+#define __WRITE_ELF_HEAD_H__
 
+/**Fonction permettant d'ecrire une en-tête dans un fichier au format ELF**/
 void write_header(FILE *f, Elf32_Ehdr elf);
 
 
--- write_elf_section_header.h
+++ write_elf_section_header.h
@@ -1,6 +1,7 @@
-#ifndef WRITE_ELF_SECT_HEAD_H
-#define WRITE_ELF_SECT_HEAD_H
+#ifndef __WRITE_ELF_SECT_HEAD_H__
+#define __WRITE_ELF_SECT_HEAD_H__
 
+/**Fonction permettant d'écrire l'entête des section**/
 void write_section_header(FILE *f, Elf32_Shdr section_tab[],uint32_t offset,uint32_t nb_sect);
 
 
